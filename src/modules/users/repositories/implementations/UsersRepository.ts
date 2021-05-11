59b1d8ed20f36ac9d439e0203d0ff7b0b725f04d
Erico Almeida <erico.silva@invillia.com>
2021-05-10 23:37:14 -0400

    added user repository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,43 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    const userCreated = Object.assign(user, { name, email });
+
+    this.users.push(userCreated);
+
+    return userCreated;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const userIndex = this.users.findIndex(
+      (user) => user.email === receivedUser.email
+    );
+
+    const user = this.users[userIndex];
+
+    user.admin = true;
+
+    this.users[userIndex] = user;
+
+    return user;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
